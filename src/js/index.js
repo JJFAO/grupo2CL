32248ca7b88fb82444db62b7821eae9a0c2bcd92
Pablo Varela Acosta <pablovarelaac@hotmail.com>
2020-02-24 12:17:59 -0300

    function "provincias y localidades" working.

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,82 @@
-// $( function() {
-//     $( "#datepicker" ).datepicker();
-//   } );
+
+// function cargaProvincias(){
+//   let listProvincias = document.getElementById('seleccionProvincia');
+  
+//   let provincias = ["Buenos Aries","Catamarca","Chaco","Chubut","Córdoba","Corrientes",
+//   "Entre Ríos","Formosa","Jujuy","La Pampa","La Rioja","Mendoza","Misiones","Neuquén","Río Negro",
+//   "Salta","San Juan","San Luis","Santa Cruz","Santa Fe","Santiago del Estero","Tierra del Fuego",
+//   "Tucumán"];
+
+// for (let i = 0; i < provincias.length ; i++){
+//   let opt = document.createElement('option');
+//   opt.innerHTML = provincias[i];
+//   listProvincias.appendChild(opt);
+// }
+// }
+// cargaProvincias();
+
+function cargaLocalidades(){
+  let provSelect = document.getElementById('seleccionProvincia');
+  let listLocalidad = document.getElementById('seleccionLocalidad');
+
+let provinciasTest={
+  tucuman: ['Burruyacu', 'Capital',  'Chicligasta',  'Cruz Alta',  'Famalla',  'Graneros',  'Juan B. Alberdi', 'La Cocha',  'Leales',  'Lules',  'Monteros',  'Rio Chico',  'Simoca',  'Tafi del Valle',  'Tafi Viejo', 'Trancas',  'Yerba Buena'],
+  buenosAires: ['CABA'],
+  cordoba: ['Cordoba', 'Rio Cuarto', 'Carlos Paz'],
+}
+
+provSelect.addEventListener('change',function(){
+  let selectOption = provinciasTest[this.value];
+  while (listLocalidad.options.length > 0){
+    listLocalidad.options.remove(0);
+  }
+
+  Array.from(selectOption).forEach(function(el){
+    let option = new Option(el, el);
+    listLocalidad.appendChild(option);
+  });
+});
+
+//     const localidades = [tucuman, buenosAires, cordoba];
+
+// for (let i=0; i<localidades.length; i++){
+//   let testLocalidades = localidades.toString()
+//   console.log(testLocalidades)
+
+//   let opt =document.createElement('option');
+//     opt.innerHTML = localidades[i];
+//   listLocalidad.appendChild(opt);
+// }
+
+}
+cargaLocalidades();
+// function cargaLocalidades(){
+//   for (let i = 0; i < listLocalidad.length; i++){
+//     let opt = document.createElement('option');
+//     opt.innerHTML = localidades[i];
+//     listLocalidad.appendChild(opt);
+//     console.log(opt)
+//     }
+  
+// }
+// cargaLocalidades();
+
+
+
 
 let d = new Date();
 let t = d.getTime();
-let datareg = t+1;
+let datareg = t + 1;
 var database = firebase.database()
 
 
-document.getElementById("formPaciente").addEventListener("submit",(e)=>{
+document.getElementById("formPaciente").addEventListener("submit", (e) => {
 
   let apPaciente = document.getElementById("apellidoPaciente").value;
   let nomPaciente = document.getElementById("nombrePaciente").value;
   let edPaciente = document.getElementById("edadPaciente").value;
   let dniPaciente = document.getElementById("documentoDNI").value;
-  let nacPaciente = document.getElementById("nacimientoPaciente").value;  
+  let nacPaciente = document.getElementById("nacimientoPaciente").value;
   let genPaciente = document.getElementById("idSexoCliente").value;
   let dirPaciente = document.getElementById("domicilioPaciente").value;
   let telFijoPaciente = document.getElementById("telefonoFijoCliente").value;
@@ -25,63 +87,36 @@ document.getElementById("formPaciente").addEventListener("submit",(e)=>{
   let cpPaciente = document.getElementById("postalCliente").value;
 
   e.preventDefault();
-  
-  function firebasePaciente(){
 
-    console.log(datareg);
-    datareg= datareg+1;
-    console.log(datareg);
+  //   function firebasePaciente(){
 
-    const obj = {
-    id: datareg,
-    Apellido: apPaciente,
-    Nombre: nomPaciente,
-    Edad: edPaciente,
-    Documento: dniPaciente,
-    FechaNacimiento: nacPaciente,
-    Genero: genPaciente,
-    Dirección: dirPaciente,
-    TelFijo: telFijoPaciente,
-    TelMovil: telMovilPaciente,
-    email: emailPaciente,
-    ciudad: ciudadPaciente,
-    Provincia: provinciaPaciente,
-    CodPostal: cpPaciente,
-  }
-  console.log(obj)
-  let db = firebase.database().ref("obj/"+datareg);
-  db.set(obj);
-}
+  //     console.log(datareg);
+  //     datareg= datareg+1;
+  //     console.log(datareg);
 
-// function firebasePaciente(apPaciente,nomPaciente,edPaciente,dniPaciente,nacPaciente,genPaciente,dirPaciente,telFijoPaciente,telMovilPaciente,emailPaciente,ciudadPaciente,provinciaPaciente,cpPaciente) {
-//   firebase.database().ref('paciente/' + datareg).set({
-//     Apellido: apPaciente,
-//     Nombre: nomPaciente,
-//     Edad: edPaciente,
-//     Documento: dniPaciente,
-//     FechaNacimiento: nacPaciente,
-//     Genero: genPaciente,
-//     Dirección: dirPaciente,
-//     TelFijo: telFijoPaciente,
-//     TelMovil: telMovilPaciente,
-//     email: emailPaciente,
-//     ciudad: ciudadPaciente,
-//     Provincia: provinciaPaciente,
-//     CodPostal: cpPaciente,
-// });
-// }
-firebasePaciente()
+  //     const obj = {
+  //     id: datareg,
+  //     Apellido: apPaciente,
+  //     Nombre: nomPaciente,
+  //     Edad: edPaciente,
+  //     Documento: dniPaciente,
+  //     FechaNacimiento: nacPaciente,
+  //     Genero: genPaciente,
+  //     Dirección: dirPaciente,
+  //     TelFijo: telFijoPaciente,
+  //     TelMovil: telMovilPaciente,
+  //     email: emailPaciente,
+  //     ciudad: ciudadPaciente,
+  //     Provincia: provinciaPaciente,
+  //     CodPostal: cpPaciente,
+  //   }
+  //   console.log(obj)
+  //   let db = firebase.database().ref("obj/"+datareg);
+  //   db.set(obj);
+  // }
 
-
-// createUsuarioPaciente(apPaciente,nomPaciente,edPaciente,dniPaciente,nacPaciente,genPaciente,dirPaciente,telFijoPaciente,telMovilPaciente,emailPaciente,ciudadPaciente,provinciaPaciente,cpPaciente){};
+  // firebasePaciente()
   formPaciente.reset();
 
 });
 
-// function createUsuarioPaciente (Apellido,Nombre,Edad,Documento,FechaNacimiento,Genero,Dirección,TelFijo,TelMovil,email,ciudad,Provincia,CodPostal){
-//  datareg+=1;
-//  const Paciente = {
-
-//  }
-//  console.log(Paciente)
-// }
