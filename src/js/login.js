a3abe76de4c38cadb543b4526e17d0fca6c1778e
Martin Sandander <martinfernando-sandander@hotmail.com>
2020-02-29 18:39:01 -0300

    validation for patient finished

--- src/js/login.js
+++ src/js/login.js
@@ -1,38 +1,36 @@
 function hideModalRecover() {
-    $('#indexModal').modal("hide");
-    $('#modalrecover').modal("show");
+  $('#indexModal').modal("hide");
+  $('#modalrecover').modal("show");
 }
 function hideModalpp() {
-    $('#ppmodal').modal("hide");
-    
-    
+  $('#ppmodal').modal("hide");
 }
 function recoverPass() {
-    Swal.fire({
+  Swal.fire({
     position: 'top-center',
     icon: 'success',
     title: 'Mensaje enviado',
     showConfirmButton: false,
     timer: 5500
-  })}
-  const usuarios = [{DNI: 28721218 , pass:"1234"}]
+  })
+}
+const usuarios = [{ DNI: 28721218, pass: "1234" }]
+function validation4Patient() {
+  let dniHtml = document.querySelector("#DNI")
+  let passHtml = document.querySelector("#passPatient")
+  for(let index = 0; index < usuarios.length; index++) {
+    const usuario = usuarios[index];
 
-  function validation4Patient () {
-     let dniHtml = document.querySelector("#DNI")
-     let passHtml = document.querySelector("#passPatient")
-     usuarios.forEach(usuario => {
-         if (dniHtml.value !== usuarios[DNI] ){
-            alert("El DNI no esta registrado")
-         }
-         usuarios.forEach(usuario => {
-            if (passHtml.value !== usuarios[pass] ){
-                alert("La contraseña no existe")
-             }
-             
-         });
-
-         
-     });
-      
-
+    if (dniHtml.value == usuario.DNI) {
+      if (passHtml.value == usuario.pass) {
+        alert("Iniciando sesion")
+        return
+      }
+      alert("La contraseña no coincide")
+      return
+    }
+    
   }
+  alert("El DNI no esta registrado")
+}
+
