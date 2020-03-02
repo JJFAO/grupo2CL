cdced30a9cd795156d2474b75ed4fc55a6c3ba6f
Pablo Varela Acosta <pablovarelaac@hotmail.com>
2020-03-01 22:26:31 -0300

    * New file "regPacienteJS" generated and connected with form. * Functions submit bloqued if client not complete form. * pop-ups generated confirm events -complete or error-, * Pop - up generated with form for error process.

---
+++ src/js/RegPaciente.js
@@ -0,0 +1,30 @@
+
+function getRegisterPac (){
+  let inputTest = document.getElementById("formPaciente").checkValidity();
+  if (inputTest !== true){
+   $('#modalFaltanDatos').modal('show')
+  }else{
+ 
+  const entrada = document.querySelectorAll('.datoPac');
+  const registroPac = {};
+    $('#modalRegistrado').modal('show')
+    entrada.forEach((datoPac) => { 
+    registroPac[datoPac.id] = datoPac.value
+  });
+  const nPacientes = JSON.parse(localStorage.getItem('Pacientes')) || [];
+  nPacientes.push(registroPac);
+  localStorage.setItem('Pacientes',JSON.stringify(nPacientes)); 
+
+}
+}
+
+
+document.getElementById("formPaciente").addEventListener("submit", (e) => {
+ e.preventDefault();
+ formPaciente.reset();
+  });
+
+  function errorEnvio(){
+      $('#modalError').modal('show');
+  }
+  
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,4 @@
 
-// function cargaProvincias(){
-//   let listProvincias = document.getElementById('seleccionProvincia');
-  
-//   let provincias = ["Buenos Aries","Catamarca","Chaco","Chubut","Córdoba","Corrientes",
-//   "Entre Ríos","Formosa","Jujuy","La Pampa","La Rioja","Mendoza","Misiones","Neuquén","Río Negro",
-//   "Salta","San Juan","San Luis","Santa Cruz","Santa Fe","Santiago del Estero","Tierra del Fuego",
-//   "Tucumán"];
-
-// for (let i = 0; i < provincias.length ; i++){
-//   let opt = document.createElement('option');
-//   opt.innerHTML = provincias[i];
-//   listProvincias.appendChild(opt);
-// }
-// }
-// cargaProvincias();
 
 function cargaLocalidades(){
   let provSelect = document.getElementById('seleccionProvincia');
@@ -49,6 +34,7 @@ let provincias={
 
 }
 
+
 provSelect.addEventListener('change',function(){
   let selectOption = provincias[this.value];
   while (listLocalidad.options.length > 0){
@@ -61,103 +47,20 @@ provSelect.addEventListener('change',function(){
   });
 });
 
-//     const localidades = [tucuman, buenosAires, cordoba];
-
-// for (let i=0; i<localidades.length; i++){
-//   let testLocalidades = localidades.toString()
-//   console.log(testLocalidades)
-
-//   let opt =document.createElement('option');
-//     opt.innerHTML = localidades[i];
-//   listLocalidad.appendChild(opt);
-// }
-
 }
 cargaLocalidades();
-// function cargaLocalidades(){
-//   for (let i = 0; i < listLocalidad.length; i++){
-//     let opt = document.createElement('option');
-//     opt.innerHTML = localidades[i];
-//     listLocalidad.appendChild(opt);
-//     console.log(opt)
-//     }
-  
-// }
-// cargaLocalidades();
 
+//max limit Date today
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1;
+let yyyy = today.getFullYear();
+ if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
 
-
-
-let d = new Date();
-let t = d.getTime();
-let datareg = t + 1;
-var database = firebase.database()
-
-
-document.getElementById("formPaciente").addEventListener("submit", (e) => {
-
-  let apPaciente = document.getElementById("apellidoPaciente").value;
-  let nomPaciente = document.getElementById("nombrePaciente").value;
-  let edPaciente = document.getElementById("edadPaciente").value;
-  let dniPaciente = document.getElementById("documentoDNI").value;
-  let nacPaciente = document.getElementById("nacimientoPaciente").value;
-  let genPaciente = document.getElementById("idSexoCliente").value;
-  let dirPaciente = document.getElementById("domicilioPaciente").value;
-  let telFijoPaciente = document.getElementById("telefonoFijoCliente").value;
-  let telMovilPaciente = document.getElementById("telefonoMovilCliente").value;
-  let emailPaciente = document.getElementById("emailPaciente").value;
-  let ciudadPaciente = document.getElementById("seleccionLocalidad").value;
-  let provinciaPaciente = document.getElementById("seleccionProvincia").value;
-  let cpPaciente = document.getElementById("postalCliente").value;
-  let checked = document.getElementById("aceptoTyC").value;
-  
-  e.preventDefault();
-
-
-
-  //   function firebasePaciente(){
-
-  //     console.log(datareg);
-  //     datareg= datareg+1;
-  //     console.log(datareg);
-
-  //     const obj = {
-  //     id: datareg,
-  //     Apellido: apPaciente,
-  //     Nombre: nomPaciente,
-  //     Edad: edPaciente,
-  //     Documento: dniPaciente,
-  //     FechaNacimiento: nacPaciente,
-  //     Genero: genPaciente,
-  //     Dirección: dirPaciente,
-  //     TelFijo: telFijoPaciente,
-  //     TelMovil: telMovilPaciente,
-  //     email: emailPaciente,
-  //     ciudad: ciudadPaciente,
-  //     Provincia: provinciaPaciente,
-  //     CodPostal: cpPaciente,
-  //   }
-  //   console.log(obj)
-  //   let db = firebase.database().ref("obj/"+datareg);
-  //   db.set(obj);
-  // }
-
-  // firebasePaciente()
-  formPaciente.reset();
-  console.log(apPaciente)
-  console.log(nomPaciente)
-  console.log(edPaciente)
-  console.log(dniPaciente)
-  console.log(nacPaciente)
-  console.log(genPaciente)
-  console.log(dirPaciente)
-  console.log(telFijoPaciente)
-  console.log(telMovilPaciente)
-  console.log(emailPaciente)
-  console.log(ciudadPaciente)
-  console.log(provinciaPaciente)
-  console.log(cpPaciente)
-  console.log(checked)
-
-});
-
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById("nacimientoPaciente").setAttribute("max", today);
