daf91a7c4d3a4a17874c92e097b1b7fdf1f3480e
Soffcsr <soff.csr@gmail.com>
2020-03-03 23:51:17 -0300

    cambio de lugar

--- src/js/admin.js
+++ src/js/admin.js
@@ -27,61 +27,61 @@
 //     `
 // }
 
-const pacientes = [
-    {nombre: 'Mariano De Filippo', edad:'20', dni:'42269845', doctor:'jj', turno:'8:00'},
-    {nombre: 'Jj', edad:'29', dni:'0958475', doctor:'luis', turno:'12:00'},
-    {nombre: 'Maria', edad:'80', dni:'43567876', doctor:'mirta', turno:'10:00'},
-    {nombre: 'Franco', edad:'9', dni:'13326907', doctor:'leandro', turno:'16:00'},
-    {nombre: 'Josefina', edad:'25', dni:'32019283', doctor:'ana', turno:'22:00'},
-    ]
-function cargarPacientes() {
-  const tablaPaciente = document.getElementById("paciente");
-  for (let i = 0; i < pacientes.length; i++) {
-    const paciente = pacientes[i];
-    tablaPaciente.innerHTML += `
-        <tr id="" class="thead-light">
-            <td>${paciente.nombre}</td>
-            <td>${paciente.edad}</td>
-            <td>${paciente.dni}</td>
-            <td>${paciente.doctor}</td>
-            <td>${paciente.turno}</td>
-            <td><button class="check2 text-success border-0 bg-light">
-            ✔
-          </button>
-          <button class="check2 text-success border-0 bg-light">
-            ❌
-          </button>
-          </td>
-        </tr>
-        `
-  }
-}
-const doctores = [
-  {nombre: 'Mariano De Filippo', dni:'42269845', Especialidad:'pediatra', Horarios:'8:00'},
-  {nombre: 'Jj', dni:'0958475', Especialidad:'cardiologo', Horarios:'12:00'},
-  {nombre: 'Maria', dni:'43567876', Especialidad:'ginecologo', Horarios:'10:00'},
-  {nombre: 'Franco', dni:'13326907', Especialidad:'urologo', Horarios:'16:00'},
-  {nombre: 'Josefina', dni:'32019283', Especialidad:'pediatra', Horarios:'22:00'},
-  ]
-function cargarDoctores() {
-const tablaDoctor = document.getElementById("paciente");
-for (let i = 0; i < doctores.length; i++) {
-  const doctor = doctores[i];
-  tablaDoctor.innerHTML += `
-      <tr id="" class="thead-light">
-          <td>${doctor.nombre}</td>
-          <td>${doctor.edad}</td>
-          <td>${doctor.dni}</td>
-          <td>${doctor.doctor}</td>
-          <td>${doctor.turno}</td>
-          <td><button class="check2 text-success border-0 bg-light">
-          ✔
-        </button>
-        <button class="check2 text-success border-0 bg-light">
-          ❌
-        </button>
-        </td>
-      </tr>
-      `
-}
-}
+// const pacientes = [
+//     {nombre: 'Mariano De Filippo', edad:'20', dni:'42269845', doctor:'jj', turno:'8:00'},
+//     {nombre: 'Jj', edad:'29', dni:'0958475', doctor:'luis', turno:'12:00'},
+//     {nombre: 'Maria', edad:'80', dni:'43567876', doctor:'mirta', turno:'10:00'},
+//     {nombre: 'Franco', edad:'9', dni:'13326907', doctor:'leandro', turno:'16:00'},
+//     {nombre: 'Josefina', edad:'25', dni:'32019283', doctor:'ana', turno:'22:00'},
+//     ]
+// function cargarPacientes() {
+//   const tablaPaciente = document.getElementById("paciente");
+//   for (let i = 0; i < pacientes.length; i++) {
+//     const paciente = pacientes[i];
+//     tablaPaciente.innerHTML += `
+//         <tr id="" class="thead-light">
+//             <td>${paciente.nombre}</td>
+//             <td>${paciente.edad}</td>
+//             <td>${paciente.dni}</td>
+//             <td>${paciente.doctor}</td>
+//             <td>${paciente.turno}</td>
+//             <td><button class="check2 text-success border-0 bg-light">
+//             ✔
+//           </button>
+//           <button class="check2 text-success border-0 bg-light">
+//             ❌
+//           </button>
+//           </td>
+//         </tr>
+//         `
+//   }
+// }
+// const doctores = [
+//   {nombre: 'Mariano De Filippo', dni:'42269845', Especialidad:'pediatra', Horarios:'8:00'},
+//   {nombre: 'Jj', dni:'0958475', Especialidad:'cardiologo', Horarios:'12:00'},
+//   {nombre: 'Maria', dni:'43567876', Especialidad:'ginecologo', Horarios:'10:00'},
+//   {nombre: 'Franco', dni:'13326907', Especialidad:'urologo', Horarios:'16:00'},
+//   {nombre: 'Josefina', dni:'32019283', Especialidad:'pediatra', Horarios:'22:00'},
+//   ]
+// function cargarDoctores() {
+// const tablaDoctor = document.getElementById("paciente");
+// for (let i = 0; i < doctores.length; i++) {
+//   const doctor = doctores[i];
+//   tablaDoctor.innerHTML += `
+//       <tr id="" class="thead-light">
+//           <td>${doctor.nombre}</td>
+//           <td>${doctor.edad}</td>
+//           <td>${doctor.dni}</td>
+//           <td>${doctor.doctor}</td>
+//           <td>${doctor.turno}</td>
+//           <td><button class="check2 text-success border-0 bg-light">
+//           ✔
+//         </button>
+//         <button class="check2 text-success border-0 bg-light">
+//           ❌
+//         </button>
+//         </td>
+//       </tr>
+//       `
+// }
+// }
