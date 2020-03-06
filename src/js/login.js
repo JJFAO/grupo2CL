4e57047e732b4be03e96fe6a62f8c5414c716587
Pablo Varela Acosta <pablovarelaac@hotmail.com>
2020-03-05 22:06:39 -0300

    function added. Button pacient show personal information about dates if have avaliable

--- src/js/login.js
+++ src/js/login.js
@@ -31,7 +31,9 @@ function recoverPass() {
     if (dniHtml.value == usuario.documento) {
       if (passHtml.value == usuario.password) {
         alert("Inicio exitoso")
-        localStorage.setItem("usuariologueado", usuariologueado)
+        localStorage.setItem("usuariologueado", JSON.stringify(usuariologueado))
+
+
         return
       }
       alert("La contraseña no coincide")
@@ -61,9 +63,65 @@ function validation4Profesional() {
   alert("El CUIT no esta registrado")
 }
 
+// function consultaTurnoPaciente (){
+//   const usuarioClinica = JSON.parse(localStorage.getItem("Pacientes"))
+//   $('#modalPacienteSesion').modal("show");
+// }
+// function pacienteProfesional (){
+//   $('#modalProfesionalSesion').modal("show");
+
+// }
+
+
+
+//testing//
+
+const pacientesForTesting = JSON.parse(localStorage.getItem("usuariologueado"));
+const usuarioForTesting = pacientesForTesting;
+
+
+    let userLogued= {
+	id : usuarioForTesting.documento,
+	nombre : usuarioForTesting.nombre,
+	apellido : usuarioForTesting.apellido,
+  // turno: usuarioForTesting.turno,
+  turno: '25 de Marzo de 2020',
+  hora: '20:00',
+  turnoTesting:'20:00 - 21:00',
+	// doctor: usuarioForTesting.doctor,
+  doctor: 'Juan Alonso',
+}
+
+
 function consultaTurnoPaciente (){
-  const usuarioClinica = JSON.parse(localStorage.getItem("Pacientes"))
-  $('#modalPacienteSesion').modal("show");
+  if (pacientesForTesting !== []){
+    if(pacientesForTesting.turnoTesting ==''){
+      $('#modalPacienteSesion').modal("show");
+      var testingModalSinTurnos = `
+            <h1>Bienvenido, ${userLogued.nombre}</h1>
+            <br>
+            <p>Usted no tiene turnos asignados.</p>
+            <p>Puede solicitar un turno haciendo click en el botón Solicitar Turno</p>
+            </div>
+            `
+            modalPacienteLogueado.innerHTML = testingModalSinTurnos;
+          }else{
+            $('#modalPacienteSesion').modal("show");
+            var testingModalConTurnos = `
+                  <h1>Bienvenido, ${userLogued.nombre}</h1>
+                  <br>
+                  <p>Usted turno el día ${userLogued.turno}.a las ${userLogued.turnoTesting} Hs</p>
+                  <p>con el doctor ${userLogued.doctor}</p>
+                  <p> Por favor, llegue 15 min antes de su turno asignado
+                  </div>
+                  `
+                  modalPacienteLogueado.innerHTML = testingModalConTurnos;
+                }
+          }
+
+// let modalDataPaciente = document.getElementById('modalPacienteLogueado')
+// $('#modalPacienteSesion').modal("show");
+
 }
 function pacienteProfesional (){
   $('#modalProfesionalSesion').modal("show");
