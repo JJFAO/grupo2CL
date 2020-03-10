5ff10372763fe250270d71cbd14598d0bba5e46e
walter celiz <walterceliz93@gmail.com>
2020-03-10 19:09:53 -0300

    admin-section

--- src/js/tablaPacientes.js
+++ src/js/tablaPacientes.js
@@ -15,7 +15,6 @@ let dni = document.querySelector("#documento")
             <td>${aPacientes.edad}</td>
             <td>${aPacientes.documento}</td>
             <td>-</td>
-            <td>-</td>
             <td><button id="checkAprob1" class="check2 text-success border-0 bg-light">
             ✔
           </button>
