b2898e6f25dae18ad5cbfab4547fba570bf505e9
marcobaiad <marcobaiad@gmail.com>
2020-03-09 22:40:45 -0300

    add modal for register succes, bad register

--- src/js/SingUpdoctor.js
+++ src/js/SingUpdoctor.js
@@ -1,29 +1,36 @@
 function getRegisterD() {
-    const registro = {};
+    let inputTest = document.getElementById("formDoctor").checkValidity();
+    if (inputTest !== true) {
+        $('#modalFaltanDatos').modal('show')
+    } else {
+        const registro = {};
+        const inputs = document.querySelectorAll('.datos');
+        inputs.forEach((dato) => {
+            registro[dato.id] = dato.value
+        });
 
-    const inputs = document.querySelectorAll('.datos');
-    inputs.forEach((dato) => {
-        registro[dato.id] = dato.value
-    });
-
-    const horarios = document.querySelectorAll('.horarios');
-    const turnos = {};
-    horarios.forEach((h) => {
-        turnos[h.id] = Array.from(h.selectedOptions).map(o => o.value);
-    });
-    registro.dias = [turnos];
-
-    console.log(registro);
-
-    const nDoctores = JSON.parse(localStorage.getItem('rDoctores')) || [];
-    nDoctores.push(registro);
-    localStorage.setItem('rDoctores',JSON.stringify(nDoctores))
+        const horarios = document.querySelectorAll('.horarios');
+        const turnos = {};
+        horarios.forEach((h) => {
+            turnos[h.id] = Array.from(h.selectedOptions).map(o => o.value);
+        });
+        registro.dias = [turnos];
+        $('#modalRegistrado').modal('show')
+        const nDoctores = JSON.parse(localStorage.getItem('rDoctores')) || [];
+        nDoctores.push(registro);
+        localStorage.setItem('rDoctores', JSON.stringify(nDoctores))
+    }
 }
 
-function errorEnvio(){
+document.getElementById("formDoctor").addEventListener("submit", (e) => {
+    e.preventDefault();
+    formDoctor.reset();
+});
+
+function errorEnvio() {
     $('#modalError').modal('show');
 }
 
-function Registrado(){
-    $('#modalRegistrado').modal('show');
-}
+function newDoc() {
+    window.location.assign("index.html")
+  }
