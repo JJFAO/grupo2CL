1e8abfd73ba51f7c4629d09b105f82f250748a59
Pablo Varela Acosta <pablovarelaac@hotmail.com>
2020-03-12 18:28:16 -0300

    redirect ok in register pacient

--- src/js/RegPaciente.js
+++ src/js/RegPaciente.js
@@ -29,7 +29,7 @@ document.getElementById("formPaciente").addEventListener("submit", (e) => {
       $('#modalError').modal('show');
   }
   function newDoc() {
-    window.location.assign("./../../index.html")
+    window.location.assign("index.html")
   }
   function sendTXT() {
     let messajeError = document.getElementById("errorEnvioTXT").value;
