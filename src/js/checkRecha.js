971e4feba35dc28a0e446e50e472c1fa8ba38453
gabriela <gonzaa1694@gmail.com>
2020-03-12 20:18:17 -0300

    finalizo admin-section

--- src/js/checkRecha.js
+++ src/js/checkRecha.js
@@ -1,13 +1,13 @@
 // funcion que aprueba doctores
 function rechazarDoc(element) {
     const resultadoNo = element.closest("tr").querySelector(".aprobado");
-    resultadoNo.innerHTML = ` No `;
+    resultadoNo.innerHTML = `No`;
     const doctores = JSON.parse(localStorage.getItem("rDoctores"));
     const posicionDoc = doctores.findIndex(
       doctor => element.id == doctor.matriculaP
     );
     console.log(element)
-    doctores[posicionDoc].autorizado = "No";
+    doctores[posicionDoc].autorizado = `No`;
     localStorage.setItem("rDoctores", JSON.stringify(doctores));
   }
   
@@ -15,10 +15,11 @@ function rechazarDoc(element) {
   // funcion que aprueba pacientes
   function rechazadoPac(element) {
     const resultadoPacNo = element.closest("tr").querySelector(".aprobadosP");
-    resultadoPacNo.innerHTML = ` No `;
+    resultadoPacNo.innerHTML = `No`;
     const pacientes = JSON.parse(localStorage.getItem("Pacientes"));
-    const posicionPac = pacientes.findIndex(aPacientes => element.id == aPacientes.documento);
-    pacientes[posicionPac].autorizado = 'No'
+    const posicionPac = pacientes.findIndex(
+      aPacientes => element.id == aPacientes.documento);
+    pacientes[posicionPac].autorizado = `No`
     localStorage.setItem('Pacientes', JSON.stringify(pacientes));
   }
   
