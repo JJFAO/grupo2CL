9434292a493c23975466f6056dd97f8d9b746b6b
Fredy <fredykv@hotmail.com>
2020-03-25 00:49:09 -0300

    delete turno

--- src/js/validationUsersLogged.js
+++ src/js/validationUsersLogged.js
@@ -1,20 +1,20 @@
-
 function consultaTurnoPaciente() {
-  if (localStorage.getItem('profelogueado') !== null) {
-    $('#modalPacienteSesion').modal("show");
-    let elimTurno = document.getElementById('elimiTurno').style.visibility = "collapse";
-    let solicTurno = document.getElementById('soliciTurno').style.visibility = "collapse";
+  if (localStorage.getItem("profelogueado") !== null) {
+    $("#modalPacienteSesion").modal("show");
+    let elimTurno = (document.getElementById("elimiTurno").style.visibility =
+      "collapse");
+    let solicTurno = (document.getElementById("soliciTurno").style.visibility =
+      "collapse");
 
-
-     function cargarListaPaciente() {
+    function cargarListaPaciente() {
       const turnosDoctor = JSON.parse(localStorage.getItem("rTurnos")) || [];
-      const docLogueado = JSON.parse(localStorage.getItem('profelogueado'));
-    
-    const mapDelTurno = turnosDoctor.filter(function(turnoDelDia){
-    return turnoDelDia.doctores === docLogueado.id
-    })
-    
-    var testingModalSinLog = `
+      const docLogueado = JSON.parse(localStorage.getItem("profelogueado"));
+
+      const mapDelTurno = turnosDoctor.filter(function(turnoDelDia) {
+        return turnoDelDia.doctores === docLogueado.id;
+      });
+
+      var testingModalSinLog = `
   <h1 class="textoProfesional" id="profesionales">Buen día, ${docLogueado.nombre}</h1>
   <p> Sus pacientes del día son: </p>
    <div id="tablaPacientes">
@@ -28,12 +28,12 @@ function consultaTurnoPaciente() {
         </tr>
       </thead>
       </table>
-      `
-     modalPacienteLogueado.innerHTML = testingModalSinLog;
-    
-    // tablaPacientes.innerHTML = "";
-    
-    for (let i = 0; i < mapDelTurno.length; i++) {
+      `;
+      modalPacienteLogueado.innerHTML = testingModalSinLog;
+
+      // tablaPacientes.innerHTML = "";
+
+      for (let i = 0; i < mapDelTurno.length; i++) {
         const turnos = mapDelTurno[i];
         tableroDePacientes.innerHTML += `
         <tr id="" class="thead-light">
@@ -46,32 +46,37 @@ function consultaTurnoPaciente() {
         `;
       }
     }
-    cargarListaPaciente()
-
-  } else if (localStorage.getItem('usuariologueado') === null) {
-    $('#modalPacienteSesion').modal("show");
-    let elimTurno = document.getElementById('elimiTurno').style.visibility = "collapse";
-    let solicTurno = document.getElementById('soliciTurno').style.visibility = "collapse";
+    cargarListaPaciente();
+  } else if (localStorage.getItem("usuariologueado") === null) {
+    $("#modalPacienteSesion").modal("show");
+    let elimTurno = (document.getElementById("elimiTurno").style.visibility =
+      "collapse");
+    let solicTurno = (document.getElementById("soliciTurno").style.visibility =
+      "collapse");
     var testingModalSinLog = `
             <h1>¡Hola!</h1>
             <br>
             <p>Por favor, Inicie Sesión .</p>
             <p>para gestionar sus datos</p>
             </div>
-            `
+            `;
     modalPacienteLogueado.innerHTML = testingModalSinLog;
   }
 
-  if (localStorage.getItem('rTurnos') === null) {
-    $('#modalPacienteSesion').modal("show");
-    let elimTurno = document.getElementById('elimiTurno').style.visibility = "collapse";
-    let solicTurno = document.getElementById('soliciTurno').style.visibility = "collapse";
-    const logueoForTesting = JSON.parse(localStorage.getItem('usuariologueado'));
+  if (localStorage.getItem("rTurnos") === null) {
+    $("#modalPacienteSesion").modal("show");
+    let elimTurno = (document.getElementById("elimiTurno").style.visibility =
+      "collapse");
+    let solicTurno = (document.getElementById("soliciTurno").style.visibility =
+      "collapse");
+    const logueoForTesting = JSON.parse(
+      localStorage.getItem("usuariologueado")
+    );
     const pacienteEntrado = logueoForTesting;
     const pacLog = {
       id: pacienteEntrado.id,
-      nombre: pacienteEntrado.nombre,
-    }
+      nombre: pacienteEntrado.nombre
+    };
 
     var testingModalSinTurnos = `
             <h1>Hola, ${pacLog.nombre}</h1>
@@ -79,38 +84,41 @@ function consultaTurnoPaciente() {
             <p>Usted no tiene turnos asignados.</p>
             <p>Puede solicitar un turno haciendo click en el botón Turno</p>
             </div>
-            `
+            `;
     modalPacienteLogueado.innerHTML = testingModalSinTurnos;
-
   } else {
-    const logueoForTesting = JSON.parse(localStorage.getItem('usuariologueado'));
-    const pacientesForTestings = JSON.parse(localStorage.getItem("rTurnos"))|| [];
+    const logueoForTesting = JSON.parse(
+      localStorage.getItem("usuariologueado")
+    );
+    const pacientesForTestings =
+      JSON.parse(localStorage.getItem("rTurnos")) || [];
     const doctoresForTestings = JSON.parse(localStorage.getItem("rDoctores"));
     //validation for pacients//
-    const modalACargar = pacientesForTestings.find(function (pacienteForTest) {
+    const modalACargar = pacientesForTestings.find(function(pacienteForTest) {
       return pacienteForTest.dniPac == logueoForTesting.id;
-    })
+    });
 
-    const profeACargar = doctoresForTestings.find(function (docTest) {
+    const profeACargar = doctoresForTestings.find(function(docTest) {
       return docTest.cuil == modalACargar.doctores;
-    })
+    });
 
     const cuilDoctor = profeACargar;
     const datoDoctor = {
       nombre: cuilDoctor.nombre,
-      apellido: cuilDoctor.apellido,
-    }
+      apellido: cuilDoctor.apellido
+    };
 
     const userLogued = {
       id: modalACargar.id,
       nombre: modalACargar.nombrePac,
-      apellido: modalACargar.apelliPopac,
-    }
+      apellido: modalACargar.apelliPopac
+    };
 
-    $('#modalPacienteSesion').modal("show");
-    let elimTurno = document.getElementById('elimiTurno').style.visibility = "show";
+    $("#modalPacienteSesion").modal("show");
+    let elimTurno = (document.getElementById("elimiTurno").style.visibility =
+      "show");
 
-    document.getElementById('soliciTurno').style.visibility = "collapse";
+    document.getElementById("soliciTurno").style.visibility = "collapse";
 
     var testingModalConTurnos = `
         <h1>Hola, ${userLogued.nombre} ${userLogued.apellido}</h1>
@@ -129,23 +137,24 @@ function consultaTurnoPaciente() {
             </thead>
             </table>
         </div>
-        `
+        `;
     modalPacienteLogueado.innerHTML = testingModalConTurnos;
 
-    const mapDelNoTurno = pacientesForTestings.filter(function(turnosDelDia){
-      return turnosDelDia.dniPac !== logueoForTesting.id
-      })
-   
-    
-    const mapDelTurno = pacientesForTestings.filter(function(turnosDelDia){
-    return turnosDelDia.dniPac === logueoForTesting.id
-    })
+    const mapDelNoTurno = pacientesForTestings.filter(function(turnosDelDia) {
+      return turnosDelDia.dniPac !== logueoForTesting.id;
+    });
 
-    console.log("Mapeo del No Turno", mapDelNoTurno)
-    console.log("Mapeo del Turno", mapDelTurno)
+    const mapDelTurno = pacientesForTestings.filter(function(turnosDelDia) {
+      return turnosDelDia.dniPac === logueoForTesting.id;
+    });
+
+    console.log("Mapeo del No Turno", mapDelNoTurno);
+    console.log("Mapeo del Turno", mapDelTurno);
 
     for (let i = 0; i < mapDelTurno.length; i++) {
       const turnos = mapDelTurno[i];
+      console.log(turnos);
+
       var listadoTurnos = `
       <tbody>
 
@@ -153,26 +162,50 @@ function consultaTurnoPaciente() {
         <td>${turnos.horario}</td>
         <td>${turnos.especialidad}</td>
         <td>${cuilDoctor.nombre} ${cuilDoctor.apellido}</td>
-        <td> <button id= "deleteEsteTurno" class="check2 text-danger border-0 bg-light">❌</button></td>       
+        <td> <button id= "deleteEsteTurno" onclick="borra_este('${turnos.doctores}', '${turnos.dniPac}','${turnos.dias}','${turnos.horario}' )" class="check2 text-danger border-0 bg-light">❌</button></td>       
       </tr>
       </tbody>
-      `
-      listTurnos.innerHTML +=listadoTurnos;
+      `;
+      listTurnos.innerHTML += listadoTurnos;
     }
-  
   }
 }
+
+function borra_este(doctores, dniPac, dias, horario) {
+  console.log(localStorage.getItem("rTurnos"));
+  const rTurnos = JSON.parse(localStorage.getItem("rTurnos")) || [];
+  console.log(rTurnos[0]);
+  const rTurnos_modif= []
+
+  for (let i = 0; i < rTurnos.length; i++) {
+    const turno = rTurnos[i];
+    if (
+      turno.horario === horario &&
+      turno.dias === dias &&
+      turno.doctores === doctores &&
+      turno.dniPac === dniPac
+    ) {
+    } else {
+      rTurnos_modif.push(turno);
+      console.log("no");      
+    }
+  }
+  console.log(rTurnos_modif);
+  localStorage.setItem('rTurnos', JSON.stringify(rTurnos_modif))
+  $("#modalPacienteSesion").modal("hide");
+}
+
 
 let botonDeleteTurno = document.querySelector("#elimiTurno");
 let eliminarTurno = false;
 
-
 if (botonDeleteTurno) {
   botonDeleteTurno.addEventListener("click", deleteTurno);
+  console.log();
+
   function deleteTurno() {
     if (eliminarTurno == false);
     // localStorage.removeItem("rTurnos");
     window.location.assign("index.html");
   }
 }
-
