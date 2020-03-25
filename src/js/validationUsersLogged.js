841895cabaafe5db4d48bae62bbc00e2523e4be5
marcobaiad <mcanelo@sesa-select.com.ar>
2020-03-25 07:02:41 -0300

    resolved conflict on validation for user logged

--- src/js/validationUsersLogged.js
+++ src/js/validationUsersLogged.js
@@ -6,48 +6,6 @@ function consultaTurnoPaciente() {
     let solicTurno = document.getElementById('soliciTurno').style.visibility = "collapse";
 
 
-<<<<<<< HEAD
-    function getLocal(key) {
-      return JSON.parse(localStorage.getItem(key))
-    };
-    getLocal()
-
-
-    const paciente = JSON.parse(localStorage.getItem("Pacientes"));
-    let apellPac = document.querySelector("#apellido");
-    let nombPac = document.querySelector("#nombre");
-    let edadPac = document.querySelector("#edad");
-    let dni = document.querySelector("#documento")
-
-    for (let i = 0; i < paciente.length; i++) {
-      const aPacientes = paciente[i];
-
-
-      var testingModalSinLog = `
-   <h1 class="textoProfesional" id="profesionales">Buen día, Profesional</h1>
-   <p> Sus pacientes del día son: </p>
-    <div id="tablaPacientes">
-     <table class="table w-100 pl-0 mt-0">
-       <thead class="thead-light">
-         <tr class="text-center" id="TablaPacientes">
-           <th scope="col" style="width: 13vw;">Horario</th>
-           <th scope="col" style="width: 13vw;">Paciente</th>
-           <th scope="col" style="width: 13vw;">Edad</th>
-           <th scope="col" style="width: 13vw;">Documento</th>
-         </tr>
-       </thead>
-       `
-      modalPacienteLogueado.innerHTML = testingModalSinLog
-      tablaPacientes.innerHTML += `
-         <tr id="TablaPacientes" class="thead-light">
-             <td>${aPacientes.apellido + " " + aPacientes.nombre}</td>
-             <td>${aPacientes.edad}</td>
-             <td>${aPacientes.documento}</td>
-         </tr>
-         `;
-
-    };
-=======
      function cargarListaPaciente() {
       const turnosDoctor = JSON.parse(localStorage.getItem("rTurnos")) || [];
       const docLogueado = JSON.parse(localStorage.getItem('profelogueado'));
@@ -90,7 +48,6 @@ function consultaTurnoPaciente() {
     }
     cargarListaPaciente()
 
->>>>>>> dec00842c9ee98fd8cfc492d0d6060f81092fb82
   } else if (localStorage.getItem('usuariologueado') === null) {
     $('#modalPacienteSesion').modal("show");
     let elimTurno = document.getElementById('elimiTurno').style.visibility = "collapse";
@@ -127,11 +84,7 @@ function consultaTurnoPaciente() {
 
   } else {
     const logueoForTesting = JSON.parse(localStorage.getItem('usuariologueado'));
-<<<<<<< HEAD
-    const pacientesForTestings = JSON.parse(localStorage.getItem("rTurnos"));
-=======
     const pacientesForTestings = JSON.parse(localStorage.getItem("rTurnos"))|| [];
->>>>>>> dec00842c9ee98fd8cfc492d0d6060f81092fb82
     const doctoresForTestings = JSON.parse(localStorage.getItem("rDoctores"));
     //validation for pacients//
     const modalACargar = pacientesForTestings.find(function (pacienteForTest) {
@@ -152,12 +105,6 @@ function consultaTurnoPaciente() {
       id: modalACargar.id,
       nombre: modalACargar.nombrePac,
       apellido: modalACargar.apelliPopac,
-<<<<<<< HEAD
-      diasTesting: modalACargar.dias,
-      turnoTesting: modalACargar.horario,
-      doctor: datoDoctor.nombre + ' ' + datoDoctor.apellido,
-=======
->>>>>>> dec00842c9ee98fd8cfc492d0d6060f81092fb82
     }
 
     $('#modalPacienteSesion').modal("show");
@@ -166,15 +113,6 @@ function consultaTurnoPaciente() {
     document.getElementById('soliciTurno').style.visibility = "collapse";
 
     var testingModalConTurnos = `
-<<<<<<< HEAD
-        <h1>Hola, ${userLogued.nombre}</h1>
-        <br>
-        <p>Usted tiene turno el día 
-        <p class="font-weight-bold">${userLogued.diasTesting}</p> 
-        <p class="font-weight-bold">entre las ${userLogued.turnoTesting} Hs</p>
-        <p class="font-weight-bold">con el doctor ${userLogued.doctor}</p>
-        <p> Por favor, llegue 15 min antes de su turno asignado
-=======
         <h1>Hola, ${userLogued.nombre} ${userLogued.apellido}</h1>
         <br>
         <p>Usted tiene turno el día </p>
@@ -190,13 +128,10 @@ function consultaTurnoPaciente() {
               </tr>
             </thead>
             </table>
->>>>>>> dec00842c9ee98fd8cfc492d0d6060f81092fb82
         </div>
         `
     modalPacienteLogueado.innerHTML = testingModalConTurnos;
 
-<<<<<<< HEAD
-=======
     const mapDelNoTurno = pacientesForTestings.filter(function(turnosDelDia){
       return turnosDelDia.dniPac !== logueoForTesting.id
       })
@@ -225,79 +160,19 @@ function consultaTurnoPaciente() {
       listTurnos.innerHTML +=listadoTurnos;
     }
   
->>>>>>> dec00842c9ee98fd8cfc492d0d6060f81092fb82
   }
 }
 
 let botonDeleteTurno = document.querySelector("#elimiTurno");
 let eliminarTurno = false;
 
-<<<<<<< HEAD
-=======
-
->>>>>>> dec00842c9ee98fd8cfc492d0d6060f81092fb82
 if (botonDeleteTurno) {
   botonDeleteTurno.addEventListener("click", deleteTurno);
   function deleteTurno() {
     if (eliminarTurno == false);
-<<<<<<< HEAD
     localStorage.removeItem("rTurnos");
-=======
     // localStorage.removeItem("rTurnos");
->>>>>>> dec00842c9ee98fd8cfc492d0d6060f81092fb82
     window.location.assign("index.html");
   }
 }
 
-<<<<<<< HEAD
-function consultaProfesional() {
-
-  // if (localStorage.getItem('profelogueado') !== null){
-
-  //   $('#modalPacienteSesion').modal("show");
-  //   let elimTurno = document.getElementById('elimiTurno').style.visibility = "collapse";
-  //   let solicTurno = document.getElementById('soliciTurno').style.visibility = "collapse";          
-
-
-  //   function getLocal(key) {
-  //     return JSON.parse(localStorage.getItem(key))};
-  //     getLocal()
-
-
-  //   const paciente = JSON.parse(localStorage.getItem("Pacientes"));
-  //   let apellPac = document.querySelector("#apellido");
-  //   let nombPac = document.querySelector("#nombre");
-  //   let edadPac = document.querySelector("#edad");
-  //   let dni = document.querySelector("#documento")
-
-  //   for (let i = 0; i < paciente.length; i++) {
-  //   const aPacientes = paciente[i];
-
-
-  //     var testingModalSinLog = `
-  //  <h1 class="textoProfesional" id="profesionales">Buen día, Profesional</h1>
-  //  <p> Sus pacientes del día son: </p>
-  //   <div id="tablaPacientes">
-  //    <table class="table w-100 pl-0 mt-0">
-  //      <thead class="thead-light">
-  //        <tr class="text-center" id="TablaPacientes">
-  //          <th scope="col" style="width: 13vw;">Horario</th>
-  //          <th scope="col" style="width: 13vw;">Paciente</th>
-  //          <th scope="col" style="width: 13vw;">Edad</th>
-  //          <th scope="col" style="width: 13vw;">Documento</th>
-  //        </tr>
-  //      </thead>
-  //      `
-  //        modalPacienteLogueado.innerHTML = testingModalSinLog
-  //        tablaPacientes.innerHTML += `
-  //        <tr id="TablaPacientes" class="thead-light">
-  //            <td>${aPacientes.apellido + " " + aPacientes.nombre}</td>
-  //            <td>${aPacientes.edad}</td>
-  //            <td>${aPacientes.documento}</td>
-  //        </tr>
-  //        `;
-
-  //    };}  
-}
-=======
->>>>>>> dec00842c9ee98fd8cfc492d0d6060f81092fb82
